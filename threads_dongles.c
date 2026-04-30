eed60ce90790eefad449e423a6e19f46b636e01b
stmaire <steffi.maire@gmail.com>
2026-04-30 09:43:22 +0200

    simple coder routine thread

--- threads_dongles.c
+++ threads_dongles.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   threads_dongles.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stmaire <stmaire@student.42.fr>            +#+  +:+       +#+        */
+/*   By: stephanie <stephanie@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 17:17:23 by stmaire           #+#    #+#             */
-/*   Updated: 2026/04/29 17:42:48 by stmaire          ###   ########.fr       */
+/*   Updated: 2026/04/30 09:36:39 by stephanie        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,69 @@ typedef struct s_dongles
     pthread_cond_t  dongles_cond;  
 } t_dongles;
 
-typedef s_struct coder
+typedef struct s_coder
 {
     int coder_id;
+    size_t time_to_compil;
     t_dongles *dongles;
-} t_coder
+} t_coder;
 
 void *compil(void *arg)
 {
     t_coder *coder = (t_coder *)arg;
-    printf("coder %d needs a dongle", &coder->coder_id);
-    pthread_mutex_lock(&coder->dongles->dongles_mutex);
-    while(&coder->dongles->dong)
-    pthread_cond_wait
+    printf("%d needs a dongle\n", coder->coder_id);
+    pthread_mutex_lock(&coder->dongles->dongles_mtx);
+    while(coder->dongles->dongles_qty <2)
+        pthread_cond_wait(&coder->dongles->dongles_cond, &coder->dongles->dongles_mtx);
+    printf("%d has taken a dongle\n", coder->coder_id);
+    printf("%d has taken a dongle\n", coder->coder_id);
+    coder->dongles->dongles_qty -=2;
+    pthread_mutex_unlock(&coder->dongles->dongles_mtx);
+    
+    printf("%d is compiling\n", coder->coder_id);
+    usleep(400000);
+
+    pthread_mutex_lock(&coder->dongles->dongles_mtx);
+    coder->dongles->dongles_qty +=2;
+    pthread_cond_broadcast(&coder->dongles->dongles_cond);
+    pthread_mutex_unlock(&coder->dongles->dongles_mtx);
+    
+    printf("%d is refactoring\n", coder->coder_id);
+    
+    free(coder);
+    return NULL;
+}
+
+int main(void)
+{
+    t_dongles dongles;
+    pthread_t thread[5];
+    int i;
+    
+    dongles.dongles_qty = 5;
+    pthread_mutex_init(&dongles.dongles_mtx, NULL);
+    pthread_cond_init(&dongles.dongles_cond, NULL);
+    i = 0;
+
+    while(i < 5)
+    {
+        t_coder *coder=malloc(sizeof(t_coder));
+        if (!coder)
+            return(-1);
+        coder->coder_id = i + 1;
+        coder->dongles = &dongles;
+        if (pthread_create(&thread[i], NULL, compil, coder) != 0)
+            return(-1);
+        i++;              
+    }
+    i = 0;
+    while(i <5)
+    {
+        pthread_join(thread[i], NULL);
+        i++;
+    }
+    pthread_cond_destroy(&dongles.dongles_cond);
+    pthread_mutex_destroy(&dongles.dongles_mtx);
     
 }
 
